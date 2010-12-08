762df297b8937a8300e9f38f2953eb9b0c21a275
unknown <Matt@.(none)>
2010-12-07 22:13:12 -0500

    Created git repo

---
+++ script.js
@@ -0,0 +1,6 @@
+
+	var e=document.createElement('script');
+	e.setAttribute('type','text/javascript');
+	e.setAttribute('charset','UTF-8');
+	e.setAttribute('src','http://ffffound.com/bookmarklet.js');
+	document.body.appendChild(e)
